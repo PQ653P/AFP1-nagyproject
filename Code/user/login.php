ef043ccbfebb4c9a042306a2b98b9fc45eb49df8
PQ653P <zsigulipuszz@gmail.com>
2020-11-18 19:53:11 +0100

    login.php javítása

--- Code/user/login.php
+++ Code/user/login.php
@@ -12,19 +12,22 @@
 
 		<?php include('errors.php'); ?>
 
+		<div class="input-group">
 			<label>Felhasználónév</label>
 			<input type="text" name="name" >
-		
+		</div>
+		<div class="input-group">
 			<label>Jelszó</label>
 			<input type="password" name="password">
-		
+		</div>
+		<div class="input-group">
 			<button type="submit" class="btn" name="login_user">Belépés</button>
-			
+		</div>
 		<p>
 			Nincs még fiókja? <a href="register.php">Regisztráció</a>
 		</p>
 	</form>
-
+<button class="menubutton" type="submit" formaction="file:///C:/xampp/htdocs/BLACKJACK/Code/menu.html"><span>MENÜ</span></button><br>
 
 </body>
 </html>
