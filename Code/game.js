e1e28dad182f92ad77a0fcd76d7c39b7f0e51ea0
Dániel Árvai <arvai96@gmail.com>
2020-12-01 17:13:40 +0100

    Javascript class használatának bevezetése

--- Code/game.js
+++ Code/game.js
@@ -1,44 +1,50 @@
-const CARD_SUITS = ['SPADES', 'HEARTS', 'DIAMONDS', 'CLUBS'];
-const CARD_VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'JACK', 'QUEEN', 'KING', 'ACE'];
-
-function card(suit, value) {
-    return {suit: suit, value: value};
+class Card {
+    constructor(suits, value) {
+        this.suits = suits;
+        this.value = value;
+    }
 }
 
-function create_deck() {
-    deck = [];
-    for (var i = 0; i < CARD_SUITS.length; i++) {
-        for (var j = 0; j < CARD_VALUES.length; j++) {
-            deck.push(card(CARD_SUITS[i], CARD_VALUES[j]));
+Card.SUITS = ['SPADES', 'HEARTS', 'DIAMONDS', 'CLUBS'];
+Card.VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+class Deck {
+    constructor() {
+        this.cards = [];
+        for (var i = 0; i < Card.SUITS.length; i++) {
+            for (var j = 0; j < Card.VALUES.length; j++) {
+                var card = new Card(Card.SUITS[i], Card.VALUES[j]);
+                this.cards.push(card);
+            }
         }
     }
-    return deck;
-}
 
-// algoritmus forrása:
-// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-function shuffle_deck(deck) {
-    var currentIndex = deck.length, temporaryValue, randomIndex;
+    shuffle() {
+        // algoritmus forrása:
+        // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+        var currentIndex = this.cards.length, temporaryValue, randomIndex;
 
-    while (currentIndex != 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
+        while (currentIndex != 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
 
-        temporaryValue = deck[currentIndex];
-        deck[currentIndex] = deck[randomIndex];
-        deck[randomIndex] = temporaryValue;
+            temporaryValue = this.cards[currentIndex];
+            this.cards[currentIndex] = this.cards[randomIndex];
+            this.cards[randomIndex] = temporaryValue;
+        }
     }
-}
-
-function draw_from_deck(deck) {
-    var card = deck.pop()
-
-    return card;
 }
 
 window.onload = function() {
     deck = create_deck();
     shuffle_deck(deck);
 
+    player_cards = [];
+    dealer_cards = [];
+
+    player_cards.push(draw_from_deck(deck));
+    dealer_cards.push(draw_from_deck(deck));
+    player_cards.push(draw_from_deck(deck));
+    dealer_cards.push(draw_from_deck(deck));
 
 }
